66da710db67c6b14bad552bd4417158e1b51ca96
Robin <robinsldp@msn.com>
2016-01-09 17:48:13 +0100

    Ajout class actualité

---
+++ model/actualites.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Robinou
+ * Date: 09/01/2016
+ * Time: 17:45
+ */
+class actualites
+{
+    private $id;
+    private $titre;
+    private $contenu;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * @param mixed $titre
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContenu()
+    {
+        return $this->contenu;
+    }
+
+    /**
+     * @param mixed $contenu
+     */
+    public function setContenu($contenu)
+    {
+        $this->contenu = $contenu;
+    }
+
+
+}
